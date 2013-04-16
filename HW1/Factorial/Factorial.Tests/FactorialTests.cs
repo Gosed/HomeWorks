f7be34059d9f91e25714c574b4845f8b114b9583
Nikita <nik0spb95@yandex.ru>
2013-04-16 12:11:22 +0400

    Factorial Added

---
+++ HW1/Factorial/Factorial.Tests/FactorialTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace Factorial.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FactorialTest()
+        {
+            Assert.AreEqual(Program.Factorial(6), 720);
+        }
+    }
+}
---
+++ HW1/Factorial/Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("! = ");
+            Console.Write(Factorial(n));
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Count factorial
+        /// </summary>
+        /// <param name="n">Number</param>
+        /// <returns>Factorial</returns>
+        public static int Factorial(int n)
+        {
+            int k = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                k = i * k;
+            }
+            return k;
+        }
+    }
+
+}
