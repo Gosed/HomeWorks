d024b1d396d44c3a2bd4f40bc22314ee14a49cbf
Nikita <nik0spb95@yandex.ru>
2013-05-06 12:35:20 +0400

    HW3 -Stack added

---
+++ HW3/Stack/Stack.Tests/ListTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stack.Tests
+{
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void PushTest()
+        {
+            StackClass stack = new StackClass(); 
+            stack.Push(7);
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void PopTest()
+        {
+            StackClass stack = new StackClass(); 
+            stack.Pop(56);
+            stack.Push(4);
+            stack.Pop(4);
+            Assert.IsTrue(stack.IsEmpty());
+            stack.Push(4);
+            stack.Pop(48);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void PrintTest()
+        {
+            StackClass stack = new StackClass();
+            stack.Print();
+        }
+    }
+}
---
+++ HW3/Stack/Stack/StackClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    class StackClass
+    {
+        /// <summary>
+        /// Building of a stack
+        /// </summary>
+        private class StackElement
+        {
+            public int Data { get; set; }
+            public StackElement Next { get; set; }
+            public StackElement(int value)
+            {
+                this.Data = value;
+                this.Next = null;
+            }
+        }
+        
+        /// <summary>
+        /// Head of a stack
+        /// </summary>
+        private StackElement head = null;
+
+        /// <summary>
+        /// Adding element to a stack
+        /// </summary>
+        public void Push(int number)
+        {
+            StackElement tmp = new StackElement(number);
+            tmp.Next = this.head;
+            this.head = tmp;
+        }
+
+        /// <summary>
+        /// Taking an element from stack
+        /// </summary>
+        public void Pop()
+        {
+            StackElement iterator = this.head;
+            if (iterator == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+            this.head = iterator.Next;
+            Console.WriteLine(iterator.Data);
+        }
+        
+        /// <summary>
+        /// Printing a stack
+        /// </summary>
+        public void Print()
+        {
+            StackElement iterator = this.head;
+            if (iterator == null)
+                Console.Write("Stack is empty");
+            while (iterator != null)
+            {
+                Console.Write(iterator.Data);
+                iterator = iterator.Next;
+            }
+        }
+
+        /// <summary>
+        /// If stack is empty
+        /// </summary>
+        /// <returns>True if empty</returns>
+        public bool IsEmpty()
+        {
+            return (this.head == null);
+        }
+    }
+}
