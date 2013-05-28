5ce31370b6f929e32520a40196aaa96a16892de6
Nikita <nik0spb95@yandex.ru>
2013-05-28 12:46:35 +0400

    HW8-2

---
+++ HW8/SetGeneric/SetGeneric.Tests/SetTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SetGeneric.Tests
+{
+    [TestClass]
+    public class SetTest
+    {
+        private Set<int> set1;
+        private Set<int> set2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            set1 = new Set<int>();
+            set2 = new Set<int>();
+        }
+        
+        [TestMethod]
+        public void TestUnion()
+        {
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+            set2.Add(4);
+            set2.Add(1);
+            set2.Add(2);
+            var set = new Set<int>();
+            set = set1.Union(set2);
+            var arr = new int[4];
+            int i = 0;
+            foreach (int x in set)
+            {
+                arr[i] = x;
+                ++i;
+            }
+            Assert.AreEqual(4, arr[0]);
+            Assert.AreEqual(1, arr[1]);
+            Assert.AreEqual(2, arr[2]);
+            Assert.AreEqual(3, arr[3]);
+        }        
+
+        [TestMethod]
+        public void TestCrossing()
+        {
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+            set2.Add(4);
+            set2.Add(1);
+            set2.Add(2);
+            var set = new Set<int>();
+            set = set1.Crossing(set2);
+            var arr = new int[2];
+            int i = 0;
+            foreach (int x in set)
+            {
+                arr[i] = x;
+                ++i;
+            }
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(2, arr[1]);
+        }
+    }
+}
---
+++ HW8/SetGeneric/SetGeneric/Set.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace SetGeneric
+{
+    public class Set<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Add element in set.
+        /// </summary>
+        /// <param name="value"></param>
+        public void Add(T value)
+        {
+            if (this.Search(value))
+            {
+                return;
+            }
+            list.Insert(index, value);
+            index++;
+        }
+
+        /// <summary>
+        /// True if there is such an element in set
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Search(T value)
+        {
+            foreach (T x in this.list)
+            {
+                if (value.Equals(x))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Delete element in set.
+        /// </summary>
+        /// <param name="value"></param>
+        public void Remove(T value)
+        {
+            if (!this.Search(value))
+            {
+                throw new Exception();
+            }
+            this.list.Remove(value);
+        }
+
+        /// <summary>
+        /// Union of two sets.
+        /// </summary>
+        /// <param name="set"></param>
+        /// <returns></returns>
+        public Set<T> Union(Set<T> set)
+        {
+            var result = new Set<T>();
+            foreach (T x in set)
+            {
+                result.Add(x);
+            }
+            foreach (T x in this.list)
+            {
+                if (!result.Search(x))
+                {
+                    result.Add(x);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Crossing of two sets.
+        /// </summary>
+        /// <param name="set"></param>
+        /// <returns></returns>
+        public Set<T> Crossing(Set<T> set)
+        {
+            var result = new Set<T>();
+            foreach (T x in set)
+            {
+                if (this.Search(x))
+                {
+                    result.Add(x);
+                }
+            }
+            return result;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private List<T> list = new List<T>();
+        private int index = 0;
+    }
+}
